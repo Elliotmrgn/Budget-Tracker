6b7617875db13780c46f28e49d6930a39e8d13ad
Elliot <elliotmrgn@gmail.com>
2020-06-29 15:52:25 -0400

    service worker update

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/db.js",
   "/styles.css",
-  "/manifest",
+  "/manifest.webmanifest",
   "/dist/bundle.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
