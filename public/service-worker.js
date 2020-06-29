106589c919e9592ea03c1e088bfc52f19f8e7581
Elliot <elliotmrgn@gmail.com>
2020-06-29 15:49:27 -0400

    added manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/db.js",
   "/styles.css",
-  "/dist/manifest.json",
+  "/manifest",
   "/dist/bundle.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
